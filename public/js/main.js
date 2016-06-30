950ad81c73dca018acb8df8909784f12f240fc14
John Henry <John.J.Henry4@gmail.com>
2016-06-30 10:59:53 -0400

    Latest Thurs Morning

--- public/js/main.js
+++ public/js/main.js
@@ -54,10 +54,157 @@ $(function initializeMap (){
       position: latLng
     });
     marker.setMap(currentMap);
+    return marker;  
   }
 
-  drawMarker('hotel', [40.705137, -74.007624]);
-  drawMarker('restaurant', [40.705137, -74.013940]);
-  drawMarker('activity', [40.716291, -73.995315]);
+
+var dataArrays = {
+  hotel: hotels,
+  restaurant: restaurants,
+  activity: activities
+}
+
+
+function itineraryMaker(title, type){
+    var htmlText = '<div class="itinerary-item"><span data-type="'+type+'" class="title">'+ title +'</span><button data-action="remove" class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
+    return htmlText;
+  }
+
+function Day () {
+  return {
+  hotel: {},
+  restaurant: {},
+  activity: {}
+}
+}
+
+var days = {
+  day1: Day()
+}
+var currentDay = 1;
+
+function listItinerary (selectText, selectType){
+  $('.list-group-'+ selectType).append(itineraryMaker(selectText, selectType));
+}
+
+$('[data-action="add"]').on('click', function(){
+  var selectType = $(this).siblings('.current-choice').data("type");
+  var selectText = $(this).siblings().find(':selected').text();
+  listItinerary(selectText, selectType);
+
+  for(var i = 0; i<dataArrays[selectType].length; i++){
+    if(dataArrays[selectType][i].name===selectText) days["day"+currentDay][selectType][selectText] = drawMarker('selectType', dataArrays[selectType][i].place.location); 
+  }
+  // if (selectType === 'hotel') {
+  //   for(i=0; i<hotels.length; i++){
+  //   if(hotels[i].name === selectText)  
+  //     Day1[selectType][selectText] = drawMarker('selectType', hotels[i].place.location); //We are working here
+  //     } 
+  // }
+  // if (selectType === 'restaurant') {
+  //   for(i=0; i<restaurants.length; i++){
+  //   if(restaurants[i].name === selectText)  
+  //     Day1[selectType][selectText] = drawMarker('selectType', restaurants[i].place.location); //We are working here
+  //     } 
+  // }
+  // if (selectType === 'activity') {
+  //   for(i=0; i<activitys.length; i++){
+  //   if(activitys[i].name === selectText)  
+  //     Day1[selectType][selectText] = drawMarker('selectType', activitys[i].place.location); //We are working here
+  //     } 
+  // }
+}) 
+
+$('ul').on('click', '.remove', function(){
+  event.preventDefault();
+  var selectType = $(this).siblings().data("type");
+  var selectText = $(this).siblings().text();
+  // console.log(hotelMarkers);
+  // console.log(selectText);
+  days["day"+currentDay][selectType][selectText].setMap(null);
+
+  // if (selectType === 'hotel') {
+  //   hotelMarkers[selectText].setMap(null);
+  //   delete hotelMarkers[selectText];
+  // }
+  // if (selectType === 'restaurant') {
+  //   restaurantMarkers[selectText].setMap(null);
+  //   delete restaurantMarkers[selectText];
+  // }
+  // if (selectType === 'activity') {
+  //   activityMarkers[selectText].setMap(null);
+  //   delete activityMarkers[selectText];    
+  // }
+$(this).siblings().remove(); 
+$(this).remove(); 
+  }) 
+
+function switchDay(prevDay){
+  console.log("called SD!");
+  $('.list-group-hotel').empty();
+  $('.list-group-restaurant').empty();
+  $('.list-group-activity').empty();
+  // var prevDay= currentDay-1;
+  // console.log(days["day"+prevDay].hotel);
+  clearGroup(days["day"+prevDay].hotel);
+  clearGroup(days["day"+prevDay].restaurant);
+  clearGroup(days["day"+prevDay].activity);
+}
+
+function reMap(type, obj){
+  for(var marker in obj){
+    obj[marker].setMap(currentMap);
+    listItinerary(marker, type);
+  }
+}
+
+function clearGroup(obj){
+  console.log("CG called!");
+  for (var marker in obj){
+    obj[marker].setMap(null);
+  }
+}
+
+$('[data-action="dayadd"]').on('click', function(){
+  currentDay+=1;
+  days["day"+currentDay] = Day();
+  htmlText = '<button data-action= "switchdays" class="btn btn-circle day-btn" id="day'+currentDay+'">'+currentDay+'</button>';
+  console.log(htmlText);
+  $(this).before(htmlText);
+  switchDay(currentDay-1);
+  $('#daydelete').text('Day '+currentDay+' X');
+})
+
+$('.day-buttons').on('click', '[data-action="switchdays"]', function(){
+  switchDay(currentDay);
+  currentDay = parseInt($(this).text());
+  console.log(currentDay);
+  dayViewChange();
+})
+
+function dayViewChange(){
+  reMap("hotel", days["day"+currentDay].hotel);
+  reMap("restaurant", days["day"+currentDay].restaurant);
+  reMap("activity", days["day"+currentDay].activity);
+  console.log("setting cd button to", currentDay);
+  $('#daydelete').text('Day '+currentDay+' X');
+}
+
+$('#daydelete').on('click', function(){
+  switchDay(currentDay);
+  
+  for(var i=currentDay; i<days.length; i++){
+    days["day"+i] = days["day"+i];
+    var id = "day"+i;
+    $("#"+id).text(i);
+
+  }
+  var oldLength = days.length + 2;
+  delete days["day"+oldLength];
+  $('#day'+oldLength).remove();
+  currentDay-=1;
+
+  dayViewChange();
+})
 
 });
